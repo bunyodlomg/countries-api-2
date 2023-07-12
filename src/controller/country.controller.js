1a32e0e28839ea5fe7a45bf526e953360cc545f2
bunyodlomg <bunyodlomg@gmail.com>
2023-07-12 11:23:03 +0500

    1

--- src/controller/country.controller.js
+++ src/controller/country.controller.js
@@ -1,9 +1,10 @@
 import db from "../config/db.js";
-import { Country } from "../models/Country.model.js";
+import {
+  Country
+} from "../models/Country.model.js";
 
 export async function getCountryAll(req, res) {
   try {
-    // const result = await db.query(`select name, population, region, capital, flagsvg, subregion, tld, borders from countries`);
     const result = await Country.findAll()
     res.json(result);
   } catch (error) {
@@ -13,39 +14,43 @@ export async function getCountryAll(req, res) {
     console.log(error.message);
   }
 }
-
 export async function getCountry(req, res) {
   try {
-    let name = req.params.name;
-    name = name.charAt(0).toUpperCase() + name.slice(1);
-    const result = await db.query(
-      `select name, population, region, capital, flagsvg, subregion, tld, borders from countries where name = '${name}'`
-    );
-    res.json(result);
-  } catch (error) {
-    res.status(400).send({
-      message: error.message
+    const result = await Country.findOne({
+      where: {
+        name: req.params.name
+      },
     });
+    res.send(result);
+  } catch (error) {
+    res.status(400).send(error.message);
   }
 }
 
-export async function editCountry(req, res) {
 
+export async function editCountry(req, res) {
   try {
     let id = req.params.id;
     id = id.charAt(0).toUpperCase() + id.slice(1);
-
-    const data = req.body;
-    if (!req.body.name.length) {
+    if (!req.params.id) {
       res.status(400).send({
-        message: "Please enter a countries name"
+        message: "Please enter a country name"
       });
     }
-    const result = await db.query(`UPDATE countries SET name = '${data.name}', population = ${data.population}, region = '${data.region}', capital = '${data.capital}', flagsvg = '${data.flagsvg}', subregion = '${data.subregion}', tld = '${data.tld}', borders = '${data.borders}' WHERE name = '${id}'`)
+    const result = Country.update({
+      capital: `${req.body.capital}`,
+      region: `${req.body.region}`,
+      population: `${req.body.population}`
+    }, {
+      where: {
+        name: id
+      }
+    })
     res.status(201).send({
       success: true,
       message: "countries updated successfully",
       result
+
     });
   } catch (error) {
     await db.query("ROLLBACK");
